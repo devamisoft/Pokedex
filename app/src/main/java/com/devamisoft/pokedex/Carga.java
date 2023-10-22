9cfa3e95d6dc664129be7dbf4d18bff2e5fef0f2
jlrodriguez24 <rodriguezacosta2496@gmail.com>
2023-10-22 14:08:45 -0500

    Initial commit

---
+++ app/src/main/java/com/devamisoft/pokedex/Carga.java
@@ -0,0 +1,14 @@
+package com.devamisoft.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Carga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+    }
+}
